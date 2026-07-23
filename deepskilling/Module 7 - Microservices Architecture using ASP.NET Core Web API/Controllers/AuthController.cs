cfcba1d726abf6ca6052b5330a627736237d59a9
K. Thushika Reddy <thushikareddy@gmail.com>
2026-07-23 20:55:53 +0530

    Update AuthController.cs

--- deepskilling/Module 7 - Microservices Architecture using ASP.NET Core Web API/Controllers/AuthController.cs
+++ deepskilling/Module 7 - Microservices Architecture using ASP.NET Core Web API/Controllers/AuthController.cs
@@ -1,7 +1,40 @@
-[HttpPost("login")]
-public IActionResult Login(LoginModel model)
+using JwtDemo.Models;
+using JwtDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtDemo.Controllers
 {
-    // Validate credentials
-    // Generate token
-    // Return token
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+
+        public AuthController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginModel model)
+        {
+            // Dummy credentials
+            if (model.Username == "admin" &&
+                model.Password == "password")
+            {
+                var token = _tokenService.GenerateToken(model.Username);
+
+                return Ok(new
+                {
+                    Message = "Login Successful",
+                    Token = token
+                });
+            }
+
+            return Unauthorized(new
+            {
+                Message = "Invalid Username or Password"
+            });
+        }
+    }
 }
